f217378511e18e82c58fe9adc019b92b7879b7e3
guest <guest@localhost>
2016-11-24 19:43:30 +0900

    審査が通るようにログダンプをコメント化した。

--- contents.js
+++ contents.js
@@ -2,8 +2,8 @@ var g_text = "";
 
 
 function handleMessage(request, sender, sendResponse) {
-  console.log("message from the background script:");
-  console.log(request.greeting);
+  //console.log("message from the background script:");
+  //console.log(request.greeting);
 	g_text = request.greeting;
   //sendResponse({response: "hi from content script"});
 }
@@ -23,7 +23,7 @@ chrome.storage.local.get(null, (res) => {
 
 function tweet_hide_check( target_element, str_orig) {
 	//var g_text = document.querySelector("#nglist").value;
-	console.log("g_text: " + g_text);
+	//console.log("g_text: " + g_text);
 
 
 	//console.log("str_orig: " + str_orig);
@@ -49,9 +49,9 @@ function tweet_hide_check( target_element, str_orig) {
 		//console.log("index="+index);
 		//console.log("index_orig="+index_orig);
 		if ( index != -1 || index_orig != -1) {
-			console.log("=== match: ngword=" + ngword + ", str_orig=" + str_orig);
+			//console.log("=== match: ngword=" + ngword + ", str_orig=" + str_orig);
 			target_element.hide();
-			console.log("=== hide:" + str);
+			//console.log("=== hide:" + str);
 		}
 	}
 };
--- options.js
+++ options.js
@@ -1,26 +1,26 @@
 function handleResponse(message) {
-  console.log("message from the content script:");
-  console.log(message.response);
+  //console.log("message from the content script:");
+  //console.log(message.response);
 }
 
 function sendMessageToTab(tabs) {
-  console.log("sendMessageToTab() start");
-	console.log("tabs="+tabs);
+  //console.log("sendMessageToTab() start");
+	//console.log("tabs="+tabs);
 	if ( tabs == undefined || tabs == null ) return;
 
-  console.log("tabs.length=" + tabs.length);
-  console.log("tabs[0].id=" + tabs[0].id);
+  //console.log("tabs.length=" + tabs.length);
+  //console.log("tabs[0].id=" + tabs[0].id);
 
     for (var i = 0; i < tabs.length; i++) {
         var tab = tabs[i];
-	console.log("tab.id="+tab.id);
-    console.log("chrome.tabs.sendMessage() start");
+	//console.log("tab.id="+tab.id);
+    //console.log("chrome.tabs.sendMessage() start");
     chrome.tabs.sendMessage(
       tab.id,
       {greeting: document.querySelector("#nglist").value},
       handleResponse
     );
-    console.log("chrome.tabs.sendMessage() end");
+    //console.log("chrome.tabs.sendMessage() end");
     }
 /*
   if (tabs.length > 0) {
@@ -36,11 +36,11 @@ function sendMessageToTab(tabs) {
 }
 
 function saveOptions(e) {
-  console.log("saveOptions() start");
+  //console.log("saveOptions() start");
   chrome.storage.local.set({
     nglist: document.querySelector("#nglist").value
   });
-  console.log("chrome.tabs.query() start");
+  //console.log("chrome.tabs.query() start");
   chrome.tabs.query({}, sendMessageToTab);
 }
 
